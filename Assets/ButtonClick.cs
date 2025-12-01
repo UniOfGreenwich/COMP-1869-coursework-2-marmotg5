d6e4c192268b6b30ad501ee119a91cc2cd1c2e48
Eni Haziri <eh9474k@gre.ac.uk>
2025-12-01 15:03:52 +0000

    fixing script

---
+++ Assets/ButtonClick.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Clickme()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+}
